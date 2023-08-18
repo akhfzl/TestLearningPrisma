3f6542cfeb5402b03d501f500753889205a5f007
akhfzl <akhfaiz14@gmail.com>
2023-08-18 10:34:16 +0700

    'serving-file'

--- graphql/index.js
+++ graphql/index.js
@@ -1,9 +1,11 @@
 const { gql } = require('apollo-server-express');
 const { merge } = require('lodash');
+const GraphQLUpload = require('graphql-upload-minimal');
 
 const users = require('./users');
 
 const typedef = gql`
+    scalar Upload
     type Query 
     type Mutation
 `
@@ -13,7 +15,9 @@ const typedefs = [
     users.typedef
 ];
 
-let resolvers = {}
+let resolvers = {
+    Upload: GraphQLUpload
+}
 resolvers = merge(
     resolvers,
     users.resolver
--- graphql/users/users.resolver.js
+++ graphql/users/users.resolver.js
@@ -1,11 +1,13 @@
 const prisma = require('../../db/prisma');
+const fs = require('fs');
+const finished = require("stream-promise");
 
 const Users = async(parent, args, ctx) => {
     const data = await prisma.user.findMany();
     return data
 }
 
-const AddUser = async(parent, {input}, ctx) => {
+const AddUser = async(parent, { input }, ctx) => {
     const adding = await prisma.user.create({
         data: {
             first_name: input.first_name,
@@ -17,11 +19,43 @@ const AddUser = async(parent, {input}, ctx) => {
     return adding
 }
 
+const AddProfile = async(parent, { input }, ctx) => {
+    // const adding = await prisma.profile.create({
+    //     data: {   
+    //         bio: input.bio,
+    //         userId: 1
+    //     }
+    // })
+    // console.log(adding)
+    const gets = await prisma.profile.findFirst({
+        where: {
+            userId: 2
+        },
+        include: {
+            user:true
+        }
+    })
+    console.log(gets)
+    return gets
+}
+
+const singleUpload = async(parent, { file }, args) => {
+    const { createReadStream, filename, mimetype, encoding } = file.file
+    const stream = createReadStream();
+    const out = require("fs").createWriteStream(`./public/${filename}`);
+    stream.pipe(out);
+    await finished(out);
+
+    return { filename, mimetype, encoding };
+}
+
 module.exports = {
     Query: {
         Users
     },
     Mutation: {
-        AddUser
+        AddUser,
+        AddProfile,
+        singleUpload
     }
 }
